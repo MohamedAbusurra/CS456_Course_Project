625ab443314c950526e1840b11124a0999d630ae
Mohamed Abusurra <mohamed.abusurra1@gmail.com>
2025-12-07 23:29:20 +0200

    wip: begin input generation and main class functionality
    - add file scanner and reader for data sets that will be read from txt file
    -create constructor for both hill climbing and simulated annealing search

--- Main.java
+++ Main.java
@@ -1,6 +1,36 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main{
 
     public static void main(String[] args){
+
+        ArrayList<Integer> testDataSets = new ArrayList<>();
+        File inputTests = new File("testRuns.txt");
+        int randomSeed = 1;
+
+        try {
+            Scanner fileScanner = new Scanner(inputTests);
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        HillClimbingAlgorithm hillClimbingAlgorithm = new HillClimbingAlgorithm(randomSeed);
+        SimulatedAnnealingAlgorithm simulatedAnnealingAlgorithm = new SimulatedAnnealingAlgorithm(randomSeed);
+
+        for(int i=0; i< testDataSets.length; i++){
+            
+
+        }
+
+
+        
+        
+
         
         
     }
--- SimulatedAnnealingAlgorithm.java
+++ SimulatedAnnealingAlgorithm.java
@@ -1,4 +1,10 @@
 public class SimulatedAnnealingAlgorithm {
+
+    int randomSeed;
+
+    public SimulatedAnnealingAlgorithm(int randomSeed){
+        this.randomSeed = randomSeed;
+    }
     
     public void performSimulatedAnnealingSearch(){
         
