bb518391b216b6fcbc42776a2853105de1c5cbaa
Mohamed Abusurra <mohamed.abusurra1@gmail.com>
2025-12-14 17:45:00 +0200

    feat: add different temperatures and cooling rates for simulated annealing in main class

--- Main.java
+++ Main.java
@@ -17,15 +17,28 @@ public class Main{
      // maximum number of iterations for simulated annealing
      int maxIterations = 4000;
 
+     // Different starting temperatures that will be used by the simulated annealing searches
+     ArrayList<Double> startingTemperature = new ArrayList<>();
+     startingTemperature.add(100.0);
+     startingTemperature.add(150.0);
+     startingTemperature.add(200.0);
+     // Different cooling rates that will be used by the simulated annealing searches
+     ArrayList<Double> coolingRate = new ArrayList<>();
+     coolingRate.add(0.90);
+     coolingRate.add(0.95);
+     coolingRate.add(0.99);
+
 
      // Builds helper method obejct used to generate random intitial timetable for each test and prints and read tests from txt file
      Helpermethods helperMethods = new Helpermethods("testRuns.txt", maxCoursesInSemester, seed, days, timeSlots);
 
+     // calls method to read tests from file and store them in an arraylist of strings
      LinkedList<CourseNode[]> tests = helperMethods.readTestsFromFile();
 
-     // calls method to read tests from file and store them in an arraylist of strings
-     ArrayList<String> list = helperMethods.readTestsFromFile();
+    //Stores results in a string to write them into the results file after algorithm searches complete
+     ArrayList<String> list = new ArrayList<>();
 
+     //Used to store which test number is being performed for each one the input tests
      int testNumberCounter = 1;
 
      TextFileWriter TextFileWriter = new TextFileWriter();
@@ -34,7 +47,11 @@ public class Main{
         System.out.println("test" + testNumberCounter);
         ArrayList<ArrayList<LinkedList<CourseNode>>> result = helperMethods.generateInitialState();
         System.out.println("Simulated Annealing");
-        //Imeplement simulated annealing
+        for (double temperature : startingTemperature){
+            for(double rate : coolingRate){
+                    //implementat execution for simulated annealing
+            }
+        }
         System.out.println("Hill CLimbing");
         HillClimbingAlgorithm hillClimbingAlgorithm = new HillClimbingAlgorithm();
         list.add(hillClimbingAlgorithm.getResults());
