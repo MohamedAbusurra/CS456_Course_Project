8d48ea92e28675c5b02ab15931c516165452d9dc
mohab <mohab1077@gmail.com>
2025-12-14 18:00:29 +0200

    edited course node

--- CourseNode.java
+++ CourseNode.java
@@ -2,11 +2,26 @@ interface CourseNodeLike {
     String getCourseId();
     int getSemester();
     int getInstructorId();
-
 }
 
+class CourseNode implements CourseNodeLike {
+    private String courseId;
+    private int semester;
+    private int instructorId;
 
-class CourseNode implements CourseNodeLike{
-    private String courseId();
-    
+    public CourseNode(String courseId, int semester, int instructorId) {
+        this.courseId = courseId;
+        this.semester = semester;
+        this.instructorId = instructorId;
+    }
+
+    @Override
+    public String getCourseId() { return courseId; }
+
+    @Override
+    public int getSemester() { return semester; }
+
+    @Override
+    public int getInstructorId() { return instructorId; }
+
 }
