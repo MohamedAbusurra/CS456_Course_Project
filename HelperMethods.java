a98369da0ae9f5fddaab5e1e110b5bf0ba3a1fb1
mohab <mohab1077@gmail.com>
2025-12-14 18:08:07 +0200

    added helpermethod constractur

--- HelperMethods.java
+++ HelperMethods.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+
 public class HelperMethods {
-    
+     private String fileName;
+    private int n; // how many lactures
+    private Random random;
+    private int day;
+    private int slot;
+
+    public HelperMethods() {
+
+    };
+
+    public HelperMethods(String fileName, int n, int seed, int day, int slot) {
+        this.fileName = fileName;
+        this.n = n;
+        this.random = new Random(seed);
+        this.day = day;
+        this.slot = slot;
+    }
 
   
 }
