0c15632cf124548193cb5636a5779227d89f3bbb
Mohamed Abusurra <mohamed.abusurra1@gmail.com>
2025-12-14 19:43:44 +0200

    feat: complete main with inline explanations of each component

--- Main.java
+++ Main.java
@@ -43,24 +43,38 @@ public class Main{
 
      TextFileWriter TextFileWriter = new TextFileWriter();
 
+     //loop through each test case
      for(CourseNode[] table : tests){
         System.out.println("test" + testNumberCounter);
+        //generate initial state as a list of linked lists calling the helper method object method
         ArrayList<ArrayList<LinkedList<CourseNode>>> result = helperMethods.generateInitialState();
         System.out.println("Simulated Annealing");
+        // execute the test case with all possible starting temperature and cooling rate possibilities
         for (double temperature : startingTemperature){
             for(double rate : coolingRate){
-                    //implementat execution for simulated annealing
+                  SimulatedAnnealingAlgorithm simulatedAnnealingAlgorithm = new SimulatedAnnealingAlgorithm(result, seed, maxIterations, temperature,rate);
+                  simulatedAnnealingAlgorithm.solve();
+                  //print results in the terminal
+                  helperMethods.printTimetable(simulatedAnnealingAlgorithm.getFinalTable());
+                  //add results to store in the txt later
+                  list.add(simulatedAnnealingAlgorithm.getResults());
             }
         }
+        //perform hill climbing search and print results in terminal and store results in list for adding to txt file later
         System.out.println("Hill CLimbing");
-        HillClimbingAlgorithm hillClimbingAlgorithm = new HillClimbingAlgorithm();
+        HillClimbingAlgorithm hillClimbingAlgorithm = new HillClimbingAlgorithm(result,seed,testNumberCounter);
         list.add(hillClimbingAlgorithm.getResults());
         helperMethods.printTimetable(hillClimbingAlgorithm.getFinalTable());
         testNumberCounter++;
         System.out.println();
-        
-
      }
+
+     // Stores all results in a string format and calls textFileWriter object method to store them in the results.txt
+     String finalReport = "";
+     for(String s : list){
+        finalReport += s;
+     }
+     TextFileWriter.writeTextToFile("results.txt",finalReport);
 
 
 
