8e85b2cd5fb563c0f0a4a86a978411e11e29f437
mohab <mohab1077@gmail.com>
2025-12-17 21:02:37 +0200

    added TextFileWriter

--- TextFileWriter.java
+++ TextFileWriter.java
@@ -1,5 +1,34 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class TextFileWriter {
 
-    //used to write text into files. implement soon
     
+    public String readTextFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String line = scanner.nextLine();
+
+            if (line.equalsIgnoreCase("END")) {
+                break;
+            }
+            sb.append(line).append(System.lineSeparator());
+        }
+
+        return sb.toString();
+    }
+
+    
+    public void writeTextToFile(String filename, String content) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write(content);
+        } catch (IOException e) {
+            System.out.println("error");
+            e.printStackTrace();
+        }
+    }
 }
